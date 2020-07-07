efd3e0c057ca1d34b1752afa35a38a366ccea481
pashsunuwar <psunuwar@qa.com>
2020-07-07 19:40:56 +0100

    learning CURD

---
+++ src/main/java/com/qa/mysql/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.mysql;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		LearningJDBC db = new LearningJDBC("root", "root");
+
+		db.OpeningTheConnecton();
+		db.Create();
+		db.Read();
+
+	}
+}
